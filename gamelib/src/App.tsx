3bba3bf2665727f30e94c9db39270c88b3257da8
ngzh-luke <kittipich.a@gmail.com>
2024-05-09 23:33:07 +0700

    adapted 51.05mins

--- gamelib/src/App.tsx
+++ gamelib/src/App.tsx
@@ -1,16 +1,13 @@
-import './App.css'
-import ListGroup from './components/listGroup'
-import Message from './message'
+import ListGroup from "./components/listGroup";
+import Message from "./message";
 
 function App() {
-
-
   return (
     <>
       <Message />
       <ListGroup></ListGroup>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- gamelib/src/components/listGroup.tsx
+++ gamelib/src/components/listGroup.tsx
@@ -1,19 +1,43 @@
+import { MouseEvent, useState } from "react";
+
+interface ListGroupProps {
+  items: Array<String>;
+  heading: String;
+}
+
 export default function ListGroup() {
   let items = ["THA", "CHN", "USA"];
-  items.pop();
-  items.pop();
-  items.pop();
-  if (items.length <= 0) {
-    return <>No item found</>;
-  }
+  const [selectedIndex, selectedIndexUpdater] = useState(-1); // arr[0] = variable, arr[1] = updater func
+  const handleClick = (event: MouseEvent, item: String, index: number) => {
+    selectedIndexUpdater(index);
+    console.log(`${item} item clicked:`, event);
+  };
+
   return (
     <>
-      <h1>List Group</h1>
-      <br />
+      <h2>List Group</h2>
+      {<br />}
+      {
+        items.length === 0 && (
+          <p>No item found!</p>
+        ) /* equivalent to render p tag if items' length is 0 */
+      }
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item.toUpperCase()}>{item}</li>
-        ))}
+        {
+          items.map((item, index) => (
+            <li
+              className={
+                selectedIndex === index
+                  ? "list-group-item active"
+                  : "list-group-item"
+              }
+              key={item.toUpperCase()}
+              onClick={(event) => handleClick(event, item, index)}
+            >
+              {item}
+            </li>
+          )) /* render n item by map them  */
+        }
       </ul>
     </>
   );
